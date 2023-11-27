1f83102974d5a0b48140c8337326e8282aa8722f
Xander <xanderreyes2005@gmail.com>
2023-11-27 08:31:53 -0500

    Test Commit: Custom Cursor
    Created a custom cursor :+1:

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 const navItems = document.getElementsByClassName('navbar-item');
+const cursor = document.querySelector('.cursor');
+
 function toggleNav() {
     for (let i = 0; i < navItems.length; i++) {
         navItems[i].classList.toggle('hidden');
     }
 }
+
+document.addEventListener('mousemove', function(event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const cursor = document.querySelector('.cursor');
+    cursor.animate({
+        left: `${mouseX}px`,
+        top: `${mouseY}px`
+    }, {duration: 250, fill: 'forwards', easing: 'ease-in-out'});
+});
+
+
