eee2de3eedd07d1407f8c1b95b30c537602d2c95
Xander <xanderreyes2005@gmail.com>
2023-11-27 11:28:24 -0500

    Commit 8: Organization
    Organized JS :+1:

--- scripts.js
+++ scripts.js
@@ -1,41 +1,46 @@
+// Select all navigation items
 const navItems = document.getElementsByClassName('navbar-item');
+
+// Select the cursor element
 const cursor = document.querySelector('.cursor');
 
+// Check if the device is mobile
 function isMobile() {
     const userAgent = navigator.userAgent.toLowerCase();
     const mobileKeywords = ['android', 'webos', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone'];
-  
+
     for (let keyword of mobileKeywords) {
         if (userAgent.includes(keyword)) {
             return true;
         }
     }
-  
+
     return false;
 }
 
+// Hide the cursor on mobile devices
 if (isMobile()) {
     cursor.style.display = 'none';
 }
 
+// Toggle navigation items visibility
 function toggleNav() {
     for (let i = 0; i < navItems.length; i++) {
         navItems[i].classList.toggle('hidden');
     }
 }
 
+// Move the cursor with mouse movement
 document.addEventListener('mousemove', function(event) {
     if (!isMobile()) {
         const mouseX = event.clientX;
         const mouseY = event.clientY;
-        const cursor = document.querySelector('.cursor');
         cursor.animate({
             left: `${mouseX}px`,
             top: `${mouseY}px`
-        }, {duration: 250, fill: 'forwards', easing: 'ease-in-out'});
+        }, { duration: 250, fill: 'forwards', easing: 'ease-in-out' });
     }
 });
-
 
 // Select all links in the document
 const links = document.querySelectorAll('a, button');
@@ -43,7 +48,6 @@ const links = document.querySelectorAll('a, button');
 // Add event listeners to each link
 links.forEach(link => {
     link.addEventListener('mouseover', function() {
-        const cursor = document.querySelector('.cursor');
         cursor.classList.add('cursor-hover');
     });
 
